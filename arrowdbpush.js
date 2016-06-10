5dd223454463a84a7d49e1c95e414c9e4699c2c1
Clément Blanco <clement@cyber-duck.co.uk>
2016-06-10 11:37:30 +0100

    JS lint and better documentation and better console output.

--- arrowdbpush.js
+++ arrowdbpush.js
@@ -17,31 +17,62 @@ var BLACKBERRY = ! ANDROID && ! IOS && (Ti.Platform.name === 'blackberry');
 function ArrowDBPush(arrowdbuid,arrowdbpwd) {
     this.arrowdbuid = arrowdbuid || false;
 	this.arrowdbpwd = arrowdbpwd || false;
-    this.token = '';
+    this.token      = '';
 }
 
-ArrowDBPush.prototype.registerDevice = function(channel_name, onReceive, onLaunched, onFocused, androidOptions, iosOptions, blackberryOptions)
+/**
+ * Register the device against ArrowDB in order to be able to receive Push Notifications depending
+ * on the platform(s) you want to support, for a specific channel.
+ *
+ * @param  String  channel_name  [required]
+ * @param  callback  onReceive  [required]
+ * @param  Object  iosOptions  [required if iOS support]
+ * @param  Object  androidOptions  [optional]
+ * @param  Object  blackberryOptions  [required if Blackberry support]
+ * @param  callback  onAndroidFocused  [optional]
+ * @param  callback  onBlackberryLaunched  [optional]
+ * @return void
+ */
+ArrowDBPush.prototype.registerDevice = function(channel_name, onReceive, iosOptions, androidOptions, blackberryOptions, onAndroidFocused, onBlackberryLaunched)
 {
     var that  = this,
         token = '';
 
+    // Those Android + Blackberry callbacks are optionals
+    onAndroidLaunched    = onAndroidLaunched || function();
+    onAndroidFocused     = onAndroidFocused || function();
+    onBlackberryLaunched = onBlackberryLaunched || function();
+
+    // Verify inputs depending on platform
+    if (IOS) {
+        if (! _.has(iosOptions, 'types') || ! _.has(iosOptions, 'categories')) {
+            throw new Error("[ArrowDBPush] Options for iOS configuration are missing.");
+        }
+    } else if (BLACKBERRY) {
+        if (! _.has(blackberryOptions, 'appId') || ! _.has(blackberryOptions, 'ppgUrl')
+            || ! _.has(blackberryOptions, 'usePublicPpg') || ! _.has(blackberryOptions, 'launchApplicationOnPush')
+        ) {
+            throw new Error("[ArrowDBPush] Options for Blackberry configuration are missing.");
+        }
+    }
+
     function deviceTokenSuccess(e) {
-        console.log('Device Token: ' + e.deviceToken);
+        console.log('[ArrowDBPush] Device Token: ' + e.deviceToken);
         token = e.deviceToken;
         that.token = token;
-        loginToArrowDB(that.arrowdbuid, that.arrowdbpwd, token,channel_name);
+        loginToArrowDB(that.arrowdbuid, that.arrowdbpwd, token, channel_name);
     }
 
     function deviceTokenError(e) {
-        console.log('Token Error: ' + e.error);
+        console.log('[ArrowDBPush] Token Error: ' + e.error);
     }
 
     function receivePush(e) {
         onReceive(e.data);
-        console.log("push notification received: " + JSON.stringify(e.data));
+        console.log("[ArrowDBPush] Push notification received: " + JSON.stringify(e.data));
     }
 
-    if (ANDROID){
+    if (ANDROID) {
         var CloudPush = require('ti.cloudpush');
         CloudPush.retrieveDeviceToken({
             success: deviceTokenSuccess,
@@ -53,8 +84,8 @@ ArrowDBPush.prototype.registerDevice = function(channel_name, onReceive, onLaunc
         CloudPush.showTrayNotificationsWhenFocused = androidOptions.showTrayNotificationsWhenFocused || false;
         CloudPush.singleCallback = androidOptions.singleCallback || true;
         CloudPush.addEventListener('callback', onReceive);
-        CloudPush.addEventListener('trayClickLaunchedApp', onLaunched);
-        CloudPush.addEventListener('trayClickFocusedApp', onFocused);
+        CloudPush.addEventListener('trayClickLaunchedApp', onAndroidLaunched);
+        CloudPush.addEventListener('trayClickFocusedApp', onAndroidFocused);
     } else if (IOS){
         // Check if the device is running iOS 8 or later
         if (parseInt(Ti.Platform.version.split(".")[0]) >= 8) {
@@ -91,27 +122,27 @@ ArrowDBPush.prototype.registerDevice = function(channel_name, onReceive, onLaunc
             usePublicPpg: blackberryOptions.usePublicPpg,
             launchApplicationOnPush: blackberryOptions.launchApplicationOnPush,
             onSessionCreated: function(e) {
-                console.log('Session Created');
+                console.log('[ArrowDBPush] Session Created');
             },
             onChannelCreated: function(e) {
-                console.log('Channel Created\nMessage: ' + e.message + '\nToken: ' + e.token);
+                console.log('[ArrowDBPush] Channel Created\nMessage: ' + e.message + '\nToken: ' + e.token);
                 token = e.token;
                 that.token = token;
-                console.log("Device Token: " + token);
+                console.log("[ArrowDBPush] Device Token: " + token);
                 loginToArrowDB(that.arrowdbuid, that.arrowdbpwd, token, channel_name);
             },
             onPushReceived: function(e) {
-                onReceive(e.data);
+                receivePush(e);
                 e.source.removeAllPushes();
             },
             onConfigError: function(e) {
-                console.log('ERROR\nTitle: ' + e.errorTitle + +'\nMsg: ' + e.errorMessage);
+                console.log('[ArrowDBPush] ERROR\nTitle: ' + e.errorTitle + +'\nMsg: ' + e.errorMessage);
             },
             onError: function(e) {
-                console.log('ERROR\nTitle: ' + e.errorTitle + +'\nMsg: ' + e.errorMessage);
+                console.log('[ArrowDBPush] ERROR\nTitle: ' + e.errorTitle + +'\nMsg: ' + e.errorMessage);
             },
             onAppOpened: function(e) {
-                onLaunched(e.data);
+                onBlackberryLaunched(e.data);
                 e.source.removePush(e.pushId);
             }
         });
@@ -120,9 +151,23 @@ ArrowDBPush.prototype.registerDevice = function(channel_name, onReceive, onLaunc
     }
 };
 
-ArrowDBPush.prototype.unsubscribeFromChannel = function(channel_name,token,onSuccess,onFail)
+/**
+ * Un-register a device from ArrowDB Push Notification Services so it won't receive any more push
+ * notificiations for a specific channel.
+ *
+ * @param  String  channel_name  [required]
+ * @param  String  token  [required]
+ * @param  callback  onSuccess  [optional]
+ * @param  callback  onFail  [optional]
+ * @return void
+ */
+ArrowDBPush.prototype.unsubscribeFromChannel = function(channel_name, token, onSuccess, onFail)
 {
-    var that=this;
+    var that = this;
+
+    onSuccess = onSuccess || function(){};
+    onFail    = onFail || function(){};
+
     Cloud.PushNotifications.unsubscribe({
         channel: channel_name,
         device_token: token
@@ -135,14 +180,24 @@ ArrowDBPush.prototype.unsubscribeFromChannel = function(channel_name,token,onSuc
     });
 };
 
+/**
+ *
+ * @return {[type]} [description]
+ */
 ArrowDBPush.prototype.getToken = function()
 {
-    return this.token;
+    var token = this.token;
+    if (_.isEmpty(token)) {
+        console.warn("[ArrowDBPush] Apparently the device token for ArrowDB push notifications is"+
+        "not set yet. You need to register your device using ArrowDBPush.registerDevice() in order"
+        +" to get a device token.");
+    }
+    return token;
 };
 
-function loginToArrowDB(arrowdbuid,arrowdbpwd,token,channel_name) {
+function loginToArrowDB(arrowdbuid, arrowdbpwd, token, channel_name) {
 	if (! arrowdbuid && ! arrowdbpwd) {
-		console.log("loginToArrowDB -> subscribe as guest");
+		console.log("[ArrowDBPush] loginToArrowDB -> subscribe as guest");
 		subscribeForPushNotifications(token, channel_name, true);
 		return;
 	}
@@ -152,25 +207,25 @@ function loginToArrowDB(arrowdbuid,arrowdbpwd,token,channel_name) {
     }, function (e) {
         if (e.success) {
             var user = e.users[0];
-            console.log("loginToArrowDB -> Status: Successful");
-            subscribeForPushNotifications(token,channel_name);
+            console.log("[ArrowDBPush] loginToArrowDB -> Status: Successful");
+            subscribeForPushNotifications(token, channel_name);
         } else {
-            console.log("loginToArrowDB -> Error :"+e.message);
+            console.log("[ArrowDBPush] loginToArrowDB -> Error :"+e.message);
         }
     });
 }
 
 function subscribeForPushNotifications(token, channel_name, subscribeAsGuest) {
 	var prams = {
-		channel : channel_name,
-		type : IOS ? 'ios' : Ti.Platform.osname, // osname return iphone / ipad on iOS
-		device_token : token
+		channel: channel_name,
+		type: IOS ? 'ios' : Ti.Platform.osname, // osname return iphone / ipad on iOS
+		device_token: token
 	};
 	var callBack = function(e) {
 		if (e.success) {
-			console.log('subscribeForPushNotifications -> Status: Successful [' + channel_name + ']');
+			console.log('[ArrowDBPush] subscribeForPushNotifications -> Status: Successful [' + channel_name + ']');
 		} else {
-			console.log('subscribeForPushNotifications -> Error ' + token + '(subscribeToServerPush) :\\n' + ((e.error && e.message) || JSON.stringify(e)));
+			console.log('[ArrowDBPush] subscribeForPushNotifications -> Error ' + token + '(subscribeToServerPush) :\\n' + ((e.error && e.message) || JSON.stringify(e)));
 		}
 	};
 	if (subscribeAsGuest) {
